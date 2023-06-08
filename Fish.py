4bb62c4683f979c6844bcbc15b6a2186b806a367
ArchSat <diff9974@gmail.com>
2023-06-08 20:06:14 +0300

    Init commit

---
+++ Fish.py
@@ -0,0 +1,97 @@
+import pyautogui as pag
+from PIL import ImageGrab
+from time import sleep
+import numpy as np
+from screeninfo import get_monitors, Monitor
+
+pag.FAILSAFE = True
+
+e_count = 0
+e_delta = 0
+bbox_pergect_fish = (0, 0, 0, 0)
+bbox_start_fish = (0, 0, 0, 0)
+
+
+def def_consts():
+    primary_monitor = None
+    for monitor in get_monitors():
+        monitor: Monitor
+        print(f'Обнаружен монитор: {monitor.name} ({monitor.width}x{monitor.height})')
+        if monitor.is_primary:
+            primary_monitor = monitor
+    if primary_monitor:
+        print(f'Основной монитор выбран: {primary_monitor.name} ({primary_monitor.width}x{primary_monitor.height})')
+    else:
+        raise NotImplementedError('Не удалось определить основной монитор')
+
+    global bbox_pergect_fish, bbox_start_fish, e_count, e_delta
+
+    if primary_monitor.width == 2560 and primary_monitor.height == 1440:
+        e_count = 126
+        e_delta = 5
+        e_size = (31, 32)
+        start_fish_x, start_fish_y = 1056, 969
+        pergect_fish_x, pergect_fish_y = 1144, 969
+
+    elif primary_monitor.width == 1920 and primary_monitor.height == 1080:
+        e_count = 65
+        e_delta = 5
+        e_size = (23, 23)
+        start_fish_x, start_fish_y = 793, 727
+        pergect_fish_x, pergect_fish_y = 858, 727
+
+    else:
+        raise NotImplementedError('Данное разрешение монитора не поддерживается!')
+
+    bbox_pergect_fish = (pergect_fish_x, pergect_fish_y, pergect_fish_x + e_size[0], pergect_fish_y + e_size[1])
+    bbox_start_fish = (start_fish_x, start_fish_y, start_fish_x + e_size[0], start_fish_y + e_size[1])
+
+
+def get_fish():
+    pag.keyDown('e')
+    sleep(.4)
+    pag.keyUp('e')
+
+
+def detect_e(img):
+    img = img.convert('L')
+    thresh = 240
+    r = img.point(lambda x: 255 if x > thresh else 0, mode='1')
+    data = np.asarray(r, dtype="int32")
+    e_detected = e_count-e_delta <= data.sum() <= e_count+e_delta
+    return e_detected
+
+
+def get_start_fish():
+    img = ImageGrab.grab(bbox=bbox_start_fish)
+    return detect_e(img)
+
+
+def get_perfect_fish():
+    img = ImageGrab.grab(bbox=bbox_pergect_fish)
+    return detect_e(img)
+
+
+def move():
+    pag.keyDown('w')
+    sleep(0.01)
+    pag.keyUp('w')
+
+    pag.keyDown('s')
+    sleep(0.01)
+    pag.keyUp('s')
+
+
+if __name__ == '__main__':
+    def_consts()
+    print("Начало работы:")
+    for time in range(3):
+        print(3 - time)
+        sleep(1)
+    while True:
+        if get_start_fish():
+            move()
+            get_fish()
+        if get_perfect_fish():
+            print("Обнаружена рыба")
+            get_fish()
